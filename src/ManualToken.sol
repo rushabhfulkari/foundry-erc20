959c3c322af2547ffcc4b4b561d79898b2a31161
rushabhfulkari <82122413+rushabhfulkari@users.noreply.github.com>
2024-09-30 13:39:28 +0530

    First Commit

---
+++ src/ManualToken.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ManualToke {
+    mapping(address => uint256) public s_balances;
+
+    function name() public pure returns (string memory) {
+        return "ManualToken";
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether;
+    }
+
+    function decimals() public pure returns (uint256) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return s_balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _amount) public returns (bool) {
+        uint256 previousBalances = balanceOf(msg.sender) + balanceOf(_to);
+        s_balances[msg.sender] -= _amount;
+        s_balances[_to] += _amount;
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalances);
+        return true;
+    }
+}
